67075226e4c194bc63e5f9d57617c2c52ead532d
Mobashshir <mobashshirmau@gmail.com>
2022-09-25 04:40:43 +0530

    Dashboard start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsernavbarComponent } from './component/common/usernavbar/usernavbar.component';
+import { UsersidebarComponent } from './component/common/usersidebar/usersidebar.component';
+import { UserComponent } from './component/user/user/user.component';
+import { MiddlecontentComponent } from './component/user/middlecontent/middlecontent.component';
+import { RightcontentComponent } from './component/user/rightcontent/rightcontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsernavbarComponent,
+    UsersidebarComponent,
+    UserComponent,
+    MiddlecontentComponent,
+    RightcontentComponent,
   ],
   imports: [
     BrowserModule,
