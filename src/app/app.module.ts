b2eae910c6a7be95ab7fcfa22b3a7b89f454d96e
Syed Abdullah <sayed.abdullah@telus.com>
2022-09-25 02:59:00 +0530

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsernavbarComponent } from './component/common/usernavbar/usernavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsernavbarComponent,
   ],
   imports: [
     BrowserModule,
